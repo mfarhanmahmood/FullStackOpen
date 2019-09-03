0a6720bfbe40a38f762da7a42f3e07c713995f18
Muhammad Farhan Mahmood <farhanmahmood433@gmail.com>
2019-09-03 22:45:18 +0500

    Added Starting code for unicafe project of Part_1

---
+++ Part_1 Introduction to React/unicafe/src/index.js
@@ -0,0 +1,18 @@
+import React, {useState} from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+    // save clicks of each button to own state
+    const [good, setGood] = useState(0)
+    const [neutral, setNeutal] = useState(0)
+    const [bad, setBad] = useState(0)
+
+    return (
+        <div>
+            Code here...
+        </div>
+    )
+}
+
+
+ReactDOM.render(<App />, document.getElementById('root'));
