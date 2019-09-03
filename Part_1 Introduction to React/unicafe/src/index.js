a342c9ccb81466c3812a7a9b0b00b5fcdd4238f4
Muhammad Farhan Mahmood <farhanmahmood433@gmail.com>
2019-09-03 22:53:21 +0500

    Step_1 of unicafe project of Part_1 is complete

--- Part_1 Introduction to React/unicafe/src/index.js
+++ Part_1 Introduction to React/unicafe/src/index.js
@@ -1,15 +1,33 @@
 import React, {useState} from 'react';
 import ReactDOM from 'react-dom';
 
+const Button = ({onClick, text}) => {
+    return <button onClick={onClick}>{text}</button>
+}
+
+
 const App = () => {
     // save clicks of each button to own state
     const [good, setGood] = useState(0)
-    const [neutral, setNeutal] = useState(0)
+    const [neutral, setNeutral] = useState(0)
     const [bad, setBad] = useState(0)
 
     return (
         <div>
-            Code here...
+            <h1>Give Feedback</h1>
+            <div>
+                <Button onClick={() => setGood(good + 1)} text="Good" />
+                <Button onClick={() => setNeutral(neutral + 1)} text="Neutral" />
+                <Button onClick={() => setBad(bad + 1)} text="Bad" />
+            </div>
+            <div>
+                <h2>Statistics</h2>
+                <div>
+                    Good: {good}<br />
+                    Neutral: {neutral}<br />
+                    Bad: {bad}<br />
+                </div>
+            </div>
         </div>
     )
 }
