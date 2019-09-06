9d2a5cdcb7c47c1a8c098dfedbba36e89d30e7ab
Muhammad Farhan Mahmood <farhanmahmood433@gmail.com>
2019-09-06 12:13:15 +0500

    Step_10 of course_info(continued) project of Part_2 is complete

--- Part_2 Communicating with server/course_info/src/index.js
+++ Part_2 Communicating with server/course_info/src/index.js
@@ -1,45 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Course from './components/Course'
 
-const Header = (props) => {
-    return (
-        <>
-            <h2>{props.courseName}</h2>
-        </>
-    )
-}
-
-const Part = (props) => {
-    return (
-        <p><strong>Name:</strong> {props.name}, <strong>Exercises:</strong> {props.exercises}</p>
-    )
-}
-
-const Content = ({parts}) => {
-
-    const total = parts.reduce((sum, curr) =>  sum + curr.exercises, 0)
-
-    return (
-        <>
-            {parts.map(
-                (part) => (
-                    <Part key={part.id} name={part.name} exercises={part.exercises}/>
-                )
-            )}
-            <p>Total of {total} exercises.</p>
-        </>
-    )
-}
-
-
-const Course = ({course}) => {
-    return (
-        <div>
-            <Header courseName = {course.name} />
-            <Content parts={course.parts} />
-        </div>
-    )
-}
 
 
 const App = () => {
