3abe53af57177973409114824e60ca1ac046a628
Muhammad Farhan Mahmood <farhanmahmood433@gmail.com>
2019-09-06 11:45:27 +0500

    Step_6 of course_info(continued)  project of Part_2 is complete

--- Part_2 Communicating with server/course_info/src/index.js
+++ Part_2 Communicating with server/course_info/src/index.js
@@ -15,14 +15,8 @@ const Part = (props) => {
     )
 }
 
-const Content = (props) => {
-    return (
-        <>
-            <Part name={props.parts[0].name} exercises={props.parts[0].exercises} />
-            <Part name={props.parts[1].name} exercises={props.parts[1].exercises} />
-            <Part name={props.parts[2].name} exercises={props.parts[2].exercises} />
-        </>
-    )
+const Content = ({parts}) => {
+    return parts.map((part) => <Part key={part.id} name={part.name} exercises={part.exercises} />)
 }
 
 
