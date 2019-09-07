3052ea25038e0d2252bc0cafc6f9470a254b9835
Muhammad Farhan Mahmood <farhanmahmood433@gmail.com>
2019-09-07 16:20:56 +0500

    Step_2 of phonebook project of Part_2 is complete

--- Part_2 Communicating with server/phonebook/src/Components/AddNumber.js
+++ Part_2 Communicating with server/phonebook/src/Components/AddNumber.js
@@ -2,6 +2,16 @@ import React from 'react'
 
 const AddNumber = ({newName, setNewName, persons, setPersons}) => {
 
+    const verifyName = () => {
+        return persons.every(person => {
+            if (newName === person.name)
+            {
+                return false
+            }
+            return true
+        });
+    }
+
     const handleOnChange = (event) => {
         setNewName(event.target.value)
         console.log(event.target.value)
@@ -14,8 +24,16 @@ const AddNumber = ({newName, setNewName, persons, setPersons}) => {
             name: newName
         }
 
+        if (!verifyName())
+        {
+            alert(`${newName} is already added in the phonebook!`)
+            return
+        }
         setPersons(persons.concat(newRecord))
+
+        event.target.name.value = ''
         setNewName('')
+        console.log('Handle Submite function completed')
     }
 
 
