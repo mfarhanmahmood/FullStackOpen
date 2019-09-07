ba17c8e5d801a5b4a7477a893c7b723017ab3e46
Muhammad Farhan Mahmood <farhanmahmood433@gmail.com>
2019-09-07 20:26:22 +0500

    Step_6 of phonebook project of Part_2 is complete

--- Part_2 Communicating with server/phonebook/src/App.js
+++ Part_2 Communicating with server/phonebook/src/App.js
@@ -1,18 +1,23 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
+import axios from 'axios'
 import AddRecord from './Components/AddRecord'
 import Records from './Components/Records'
 import Filter from './Components/Filter'
 
 const App = () => {
-    const [persons, setPersons] = useState([
-        { name: 'Arto Hellas', number: '040-123456' },
-        { name: 'Ada Lovelace', number: '39-44-5323523' },
-        { name: 'Dan Abramov', number: '12-43-234345' },
-        { name: 'Mary Poppendieck', number: '39-23-6423122' }
-    ])
+    const [persons, setPersons] = useState([])
     const [newName, setNewName] = useState('')
     const [newNumber, setNewNumber] = useState('')
     const [filterTerm, setFilterTerm] = useState('')
+
+    useEffect(() => {
+        axios
+            .get('http://localhost:3001/persons')
+            .then(response => {
+                setPersons(response.data)
+            })
+    }, [])
+
     
     const filteredRecords = persons.filter(
         (person) => {
